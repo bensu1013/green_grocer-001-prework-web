d376d532f25cc78d7b18a52db05dc22d693cdee5
bensu1013 <bensu1013@hotmail.com>
2016-05-04 19:05:17 +0000

    nitrous-backup at Wed May  4 19:05:17 UTC 2016

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,48 @@
 def consolidate_cart(cart:[])
-  # code here
+  grocery = Hash.new
+  cart.each {|item|
+    item.each {|name, hash|
+      item_count = 0
+      if grocery.has_key?(name) == false
+        hash_count = {count: 1}
+        grocery[name] = hash.merge(hash_count)
+      else
+        grocery[name][:count] += 1
+      end
+      }
+    }
+  grocery
 end
 
 def apply_coupons(cart:[], coupons:[])
-  # code here
+  h = cart
+  coupons.each {|coup|
+    if cart.has_key?(coup[:item])
+      if h.has_key?(coup[:item] + " W/COUPON")
+        if (cart[coup[:item]][:count] >= coup[:num])
+          h[coup[:item] + " W/COUPON"][:count] += 1
+          cart[coup[:item]][:count] -= coup[:num]
+        end
+      else
+        if (cart[coup[:item]][:count] >= coup[:num])
+          cart[coup[:item]][:count] -= coup[:num]
+          apply_coup = Hash.new
+          apply_coup[coup[:item] + " W/COUPON"] = {:price => coup[:cost], :clearance => cart[coup[:item]][:clearance], :count => 1}
+          h.merge(apply_coup)
+        end
+      end
+    end
+    }
+  h
 end
 
 def apply_clearance(cart:[])
-  # code here
+  cart.each {|item|
+    if item[:clearance] == true
+      item[:price] -= item[:price] / 5
+    end
+    }
+  cart
 end
 
 def checkout(cart: [], coupons: [])
